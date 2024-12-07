44325a80397381f54054f3abdf4880111cb6cbca
javv1978 <jesusvasquez1978@gmail.com>
2024-12-07 15:41:55 +0100

    v3

--- PrimerEjercicio.py
+++ PrimerEjercicio.py
@@ -1,2 +1,18 @@
 
 print ("hola mundo")
+# Solicitar las notas al usuario
+nota1 = float(input("Introduce la primera nota: "))
+nota2 = float(input("Introduce la segunda nota: "))
+nota3 = float(input("Introduce la tercera nota: "))
+
+# Calcular el promedio
+promedio = (nota1 + nota2 + nota3) / 3
+
+# Mostrar el resultado
+print("El promedio de las tres notas es:", promedio)
+
+if promedio >= 5:
+    print(" alumni aprobado")
+else:
+    print(" alumni reprobafo")
+
---
+++ ejercio.py
@@ -0,0 +1,10 @@
+
+
+edad = int(input("ingresse la edad"))
+
+
+if edad < 18:
+    print("menor de edad")
+else:
+    print("mayir de edad")
+
